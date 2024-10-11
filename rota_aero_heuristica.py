a7d408f0371310f13ce5ef48883176cffdc62b86
Guilherme Augusto Rodrigues Silva <2021103070027@aluno.ifro.edu.br>
2024-10-10 20:40:26 -0400

    Gerado o arquivo test_tempo.py para verificar o tempo de execução dos códigos e rota_aero_heuristica.py com anotações para a produção do proximo código

---
+++ rota_aero_heuristica.py
@@ -0,0 +1,14 @@
+##### Anotações #####
+#Ler o grafo com distância (Calcular a distância) e depois descobrir a menor distância.
+
+#Ex. CGB----BRU----JPR
+#    CGB----BSB----JPR
+
+# for vizinho in grafo[no]
+#{'CGB'{'SP'1000KM, 'GRU'500KM}}
+#Armazenar dinstância em grafo
+
+#https://openflights.org/data.php
+
+#heuristica e Dijkstra
+##### Anotações #####
